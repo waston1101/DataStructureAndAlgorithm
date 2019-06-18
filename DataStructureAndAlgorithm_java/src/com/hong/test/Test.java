555c9aefd5d606e283e17375097fe80e3cbae694
mingchu <anye2009@aliyun.com>
2019-06-18 13:51:25 +0800

    commit

--- DataStructureAndAlgorithm_java/src/com/hong/test/Test.java
+++ DataStructureAndAlgorithm_java/src/com/hong/test/Test.java
@@ -1,5 +1,7 @@
 package com.hong.test;
 
+import java.util.LinkedList;
+
 import com.hong.dataTools.ToolsForTestCase;
 
 public class Test {
@@ -7,7 +9,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Algorithm, treat me softly, please.");
 		// ToolsForTestCase.generateOrderedArray(377, -152, 30, true, 1);
-		ToolsForTestCase.generateRandomString(32L, false, true, true, 0);
+		
+		//ToolsForTestCase.generateRandomString(32L, false, true, true, 0);
+		
+
 	}
 
 }
